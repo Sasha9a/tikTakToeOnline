578646b86b6333c198d885c6e021f0d0f2b9d66c
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-02-15 00:52:31 +0300

    Добавил css

--- components/game/game-cell.jsx
+++ components/game/game-cell.jsx
@@ -1,13 +1,13 @@
-import styles from './game.module.css';
 import {GameSymbol} from './game-symbol';
+import {clsx} from 'clsx';
 
 export function GameCell({isWinner, onClick, symbol}) {
-	return (
-		<button
-			className={`${styles['cell']} ${isWinner ? styles['cell--win'] : ''}`}
-			onClick={onClick}
-		>
-			{symbol ? <GameSymbol symbol={symbol}/> : null}
-		</button>
-	)
+  return (
+    <button
+      className={clsx('border border-gray-400 -ml-px -mt-px flex items-center justify-center', isWinner && 'bg-red-400')}
+      onClick={onClick}
+    >
+      {symbol ? <GameSymbol symbol={symbol}/> : null}
+    </button>
+  )
 }
