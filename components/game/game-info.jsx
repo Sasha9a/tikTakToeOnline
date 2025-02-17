66d11dfd49f2ea4bad58289caa3bd92da6a0e791
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-02-17 11:55:04 +0300

    Добавил линтер

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,26 +1,22 @@
-import styles from './game.module.css';
-import {GameSymbol} from './game-symbol';
+import styles from "./game.module.css";
+import { GameSymbol } from "./game-symbol";
 
-export function GameInfo({isDraw, winnerSymbol, currentStep}) {
-	if (isDraw) {
-		return (
-			<div className={styles['game-info']}>
-				Ничья
-			</div>
-		)
-	}
+export function GameInfo({ isDraw, winnerSymbol, currentStep }) {
+  if (isDraw) {
+    return <div className={styles["game-info"]}>Ничья</div>;
+  }
 
-	if (winnerSymbol) {
-		return (
-			<div className={styles['game-info']}>
-				Победитель: <GameSymbol symbol={winnerSymbol}/>
-			</div>
-		)
-	}
+  if (winnerSymbol) {
+    return (
+      <div className={styles["game-info"]}>
+        Победитель: <GameSymbol symbol={winnerSymbol} />
+      </div>
+    );
+  }
 
-	return (
-		<div className={styles['game-info']}>
-			Ход: <GameSymbol symbol={currentStep}/>
-		</div>
-	)
+  return (
+    <div className={styles["game-info"]}>
+      Ход: <GameSymbol symbol={currentStep} />
+    </div>
+  );
 }
