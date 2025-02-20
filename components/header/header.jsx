5ba7fbb5dc87c3fe752bbf1a10bed0f1209e8a90
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-02-21 01:09:01 +0300

    Вариации игры

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -13,7 +13,7 @@ export function Header() {
         Играть
       </UiButton>
       <button className="ml-auto flex items-center gap-2 text-start cursor-pointer text-teal-600">
-        <Profile />
+        <Profile name="Paromovevg" rating="1230" />
         <ArrowDownIcon />
       </button>
     </header>
