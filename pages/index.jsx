0418b7101e7eb0a15325d86be112fdbc6b5c483c
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-02-23 21:14:17 +0300

    Модальное окно

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/header';
 import { GameField, GameInfo, GameSymbol, GameTitle, useGameState } from '../components/game';
 import { useState } from 'react';
+import { UiButton, UiModal } from '../components/uikit';
 
 export default function HomePage() {
   const [playersCount] = useState(2);
@@ -23,6 +24,22 @@ export default function HomePage() {
             <GameSymbol symbol={winnerSymbol} />
           </div>
         )}
+        <UiModal width="md" isOpen={!!winnerSymbol} onClose={() => console.log('close')}>
+          <UiModal.Header>Игра завершена!</UiModal.Header>
+          <UiModal.Body>
+            <div className="text-sm">
+              Победитель: <span className="text-teal-600">Paromovevg</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size="md" variant="outline">
+              Вернуться
+            </UiButton>
+            <UiButton size="md" variant="primary">
+              Играть снова
+            </UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField
           playersCount={playersCount}
           cells={cells}
