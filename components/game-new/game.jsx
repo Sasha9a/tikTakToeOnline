0017b3d10efc583e3e2a8f64399a38f46f696182
Aleksandr <aleksandr_dovgiyy@mail.ru>
2025-03-01 13:49:50 +0300

    Рефакторинг

--- components/game-new/game.jsx
+++ components/game-new/game.jsx
@@ -5,23 +5,33 @@ import { GameInfo } from './ui/game-info';
 import { PLAYERS } from './constants';
 import { PlayerInfo } from './ui/player-info';
 import { GameMoveInfo } from './ui/game-move-info';
-import { useGameState } from './model/use-game-state';
 import { GameCell } from './ui/game-cell';
 import { GameOverModal } from './ui/game-over-modal';
+import { GAME_STATE_ACTIONS, gameStateReducer, initGameState } from './model/game-state-reducer';
+import { useReducer } from 'react';
+import { computeWinner } from './model/compute-winner';
+import { getNextMove } from './model/get-next-move';
+import { computeWinnerSymbol } from './model/compute-winner-symbol';
 
 const PLAYERS_COUNT = 2;
 
 export function Game() {
-  const { cells, currentMove, nextMove, handleCellClick, winnerSequence, winnerSymbol } = useGameState(PLAYERS_COUNT);
+  const [gameState, dispatch] = useReducer(gameStateReducer, { playersCount: PLAYERS_COUNT }, initGameState);
+
+  const winnerSequence = computeWinner(gameState);
+  const nextMove = getNextMove(gameState);
+  const winnerSymbol = computeWinnerSymbol(gameState, { winnerSequence, nextMove });
 
   const winnerPlayer = PLAYERS.find((player) => player.symbol === winnerSymbol);
+
+  const { cells, currentMove } = gameState;
 
   return (
     <>
       <GameLayout
         backLink={<BackLink />}
         title={<GameTitle />}
-        gameInfo={<GameInfo isRatingGame playersCount={4} timeMode={'1 мин. на ход'} />}
+        gameInfo={<GameInfo isRatingGame playersCount={PLAYERS_COUNT} timeMode={'1 мин. на ход'} />}
         playersList={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
           <PlayerInfo
             key={index}
@@ -41,7 +51,10 @@ export function Game() {
             isWinner={winnerSequence?.includes(index)}
             disabled={!!winnerSymbol}
             onClick={() => {
-              handleCellClick(index);
+              dispatch({
+                type: GAME_STATE_ACTIONS.CELL_CLICK,
+                index
+              });
             }}
           />
         ))}
